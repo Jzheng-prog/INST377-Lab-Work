61a137042f43eb702189077c5aee34b32159b2a6
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2022-10-21 20:11:08 -0400

    bug
    fix typo

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -11,7 +11,7 @@
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 
-function getRandiimIntInclusive(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + 1);
