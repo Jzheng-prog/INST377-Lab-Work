5a00d5cbe1f4cb9257b35690991298c174e59a0e
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2022-11-18 16:37:36 -0500

    map
    map

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -85,6 +85,12 @@ function filterList(array, filterInputValue) {
 function innitMap(){
   console.log('innitMap');
   const map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map); 
+  return map;
 }
 async function mainEvent() {
   /*
@@ -93,7 +99,7 @@ async function mainEvent() {
         When you're not working in a heavily-commented "learning" file, this also is more legible
         If you separate your work, when one piece is complete, you can save it and trust it
     */
-  innitMap();
+  const pageMap = innitMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
