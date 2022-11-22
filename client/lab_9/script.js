786463e19e83c1e8ffb0a068c06f2901070784fa
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2022-11-22 00:02:18 -0500

    Update script.js
    syntax

--- client/lab_9/script.js
+++ client/lab_9/script.js
@@ -87,8 +87,8 @@ function initChart(chart) {
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset'
-      backgroundColor: 'rgb(255, 99, 132',
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
       data: [0, 10, 5, 2, 20, 30, 45]
     }]
